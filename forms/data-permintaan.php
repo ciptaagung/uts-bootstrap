f14c4f612cb7ef317cede0895e654af1051f1511
ciptaagung <94789066+ciptaagung@users.noreply.github.com>
2024-02-22 10:10:50 +0700

    Add files via upload

---
+++ forms/data-permintaan.php
@@ -0,0 +1,13 @@
+<?php
+$name=$_POST['name'];
+echo("Nama : ".$name."<br>");
+
+$email=$_POST['email'];
+echo("Email : ".$email."<br>");
+
+$phone=$_POST['phone'];
+echo("No. Telepon : ".$phone."<br>");
+
+$alamat=$_POST['alamat'];
+echo("Alamat : ".$alamat."<br>");
+?>
